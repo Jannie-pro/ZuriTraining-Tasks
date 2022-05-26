bd0a16450d2b18cf4336e42003ee3e5d94855ff4
Janet Ibe <ifyjanette@gmail.com>
2022-05-26 13:53:39 +0100

    Created an anagram
    description

---
+++ findingAnagram.py
@@ -0,0 +1,17 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+
+    if(sorted(word) == sorted(anagram)):
+      print("The words are anagrams")
+    else:
+          print("The words aren't anagrams")
+
+word = input("Enter first word: ")
+anagram = input("Enter second word: ")
+find_anagram(word, anagram)
